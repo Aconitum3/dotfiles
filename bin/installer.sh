57fd42332a46d7d7bb9d6a50655f186faac65629
Aconitum3 <87516961+Aconitum3@users.noreply.github.com>
2023-09-23 11:35:45 +0900

    Update installer.sh

--- bin/installer.sh
+++ bin/installer.sh
@@ -22,41 +22,11 @@ if !(type "nvim" > /dev/null 2>&1); then
   ln -s /squashfs-root/AppRun /usr/bin/nvim
 fi
 
-# Backup dotfiles
-BACKUPDIR=$HOME/dotbackup
-if [ ! -d $BACKUPDIR ]; then
-  echo "Backup directory of dotfiles is not found."
-  mkdir $BACKUPDIR
-  echo "${BACKUPDIR} is created."
-
-  cd $HOME
-  for f in .??*; do
-    cp "$f" $BACKUPDIR
-    echo "$f backed up."
-  done
-fi
-
 # Check GIT
 if !(type "git" > /dev/null 2>&1); then
   echo "git is not found. Install git."
   apt install git
 fi
-
-# Clone dotfiles
-DOTFILESDIR=$HOME/git/dotfiles
-if [ ! -d $DOTFILESDIR ]; then
-  echo "Clone dotfiles..."
-  git clone https://github.com/aconitum3/dotfiles.git $HOME/git/dotfiles
-fi
-
-# Post Symbolic Links
-cd $DOTFILESDIR
-for f in .??*; do
-  [ "$f" = ".git" ] && continue
-  [ "$f" = ".DS_Store" ] && continue
-
-  ln -snfv ${DOTFILESDIR}/"$f" $HOME
-done 
 
 # Install locales
 if !(locale -a | grep -E "en_US.utf8|en_US.UTF-8" > /dev/null); then
@@ -64,11 +34,6 @@ if !(locale -a | grep -E "en_US.utf8|en_US.UTF-8" > /dev/null); then
  apt install locales
  locale-gen en_US.UTF-8
 fi
-
-# Install vim-plug
-curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
 
 # Install prerequisite packages for nvim plugins
 
